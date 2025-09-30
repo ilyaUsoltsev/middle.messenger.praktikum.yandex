2f41db8f1d93ccf03cbe5868e48beb213272b606
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2025-09-30 13:03:51 +0200

    Add profile page

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { Input } from './input';
 export { Chat } from './chat';
 export { Dialog } from './dialog';
 export { Container } from './container';
+export { BackButton } from './back-button';
