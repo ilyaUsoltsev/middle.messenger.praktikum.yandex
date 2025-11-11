00fe7f80d4503de2849765a6490dcc4a1e77efb6
Ilya Usoltsev <ilya.usoltsev@zhinst.com>
2025-11-11 12:35:15 +0100

    Update router paths: change registration route to '/sign-up' and adjust profile routes to '/settings' and '/settings/update'

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,10 +3,10 @@ export const APP_ROOT_ELEMENT = "#app";
 export enum ROUTER {
   main = "/",
   login = "/login",
-  register = "/register",
+  register = "/sign-up",
   notFound = "/404",
-  profile = "/profile",
-  profileUpdate = "/profile/update",
+  profile = "/settings",
+  profileUpdate = "/settings/update",
   password = "/password",
   chats = "/chats",
   error = "/error",
